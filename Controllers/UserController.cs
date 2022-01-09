cde6f82d44589d8c162eb0e1e61d5a837ebe9546
zandorax <zandorax@protonmail.com>
2022-01-09 13:51:59 +0100

    get user finished

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -28,22 +28,7 @@ public class UserController : ControllerBase
         
         string responseString = await response.Content.ReadAsStringAsync();
         List<User> users = JsonConvert.DeserializeObject<List<User>>(responseString);
-        /*zum debugen
-        foreach(User Username in users)
-            {
-                Console.WriteLine(Username.Id);
-                Console.WriteLine(Username.IsActive);
-                Console.WriteLine(Username.IsApiUser);
-                Console.WriteLine(Username.IsExternal);
-                Console.WriteLine(Username.Username);
-                Console.WriteLine(Username.Email);
-                Console.WriteLine(Username.PhoneNumber);
-                Console.WriteLine(Username.FallbackContact);
-                Console.WriteLine(Username.LastActivity);
-                Console.WriteLine();
-                Console.WriteLine("*****************************");
-                Console.WriteLine();
-            }*/
+        
         return users;
     }
     [HttpGet("type{id}&filter{filter}")]
