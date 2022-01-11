19105db603235655f43366732a5af8fa2006a164
Dominic Blattner <zandorax@protonmail.com>
2022-01-11 17:40:45 +0100

    Updated GetRequest.cs

--- Utility/GetRequest.cs
+++ Utility/GetRequest.cs
@@ -6,14 +6,12 @@ public static class GetRequest
 {
     public static async Task<string> getAsync(string apiParam)
     {
-        await BotAPI.Utility.Login.LoginAsync();
-
-        BotAPI.Utility.Login.client.DefaultRequestHeaders.Accept.Clear();
-        BotAPI.Utility.Login.client.DefaultRequestHeaders.Accept.Add(
+        SvcAuthentication.Client.DefaultRequestHeaders.Accept.Clear();
+        SvcAuthentication.Client.DefaultRequestHeaders.Accept.Add(
         new MediaTypeWithQualityHeaderValue("application/json"));
 
-        string uri = "http://sautervisioncenter.demo.sauter-bc.com/VisionCenterApiService/api/"+apiParam;
-        var response = await BotAPI.Utility.Login.client.GetAsync(uri);
+        string uri = "http://sautervisioncenter.demo.sauter-bc.com/VisionCenterApiService/api/" + apiParam;
+        var response = await BotAPI.Utility.SvcAuthentication.Client.GetAsync(uri);
         
         string responseString = await response.Content.ReadAsStringAsync();
         return responseString;
