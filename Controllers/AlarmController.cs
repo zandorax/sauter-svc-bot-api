97ac288230c3480c16e950239b1b850f130e03a2
Dominic Blattner <zandorax@protonmail.com>
2022-01-12 14:21:48 +0100

    GetHistoricAlarm added

--- Controllers/AlarmController.cs
+++ Controllers/AlarmController.cs
@@ -17,10 +17,24 @@ public class AlarmController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<List<Alarm>>> GetActiveAlarm()
     {
-        Task<string> taskString = SvcConnector.GetAsync("ActiveAlarm");
+        var uri = "ActiveAlarm";
+        Task<string> taskString = SvcConnector.GetAsync(uri);
         string responseString = taskString.Result;
         List<Alarm> alarms = JsonConvert.DeserializeObject<List<Alarm>>(responseString);
         
         return alarms;
+    }
+
+    [HttpGet("dateFrom{unixFrom:long}&dateTo{unixTo:long}&pageNumber{pageNbr:int}&itemsPerPage{items:int}")]
+    public async Task<ActionResult<List<Alarm>>> GetHistoricAlarm(long? unixFrom = null, long? unixTo = null, int? pageNbr = null, int? items = null)
+    {
+        var uri =
+            "HistoricAlarm?options.dateFrom="+unixFrom+"&options.dateTo="+unixTo+"&options.pageNumber="+pageNbr+"&options.itemsPerPage="+items;
+        Task<string> taskString = SvcConnector.GetAsync(uri);
+        string responseString = taskString.Result;
+        List<Alarm> alarms = JsonConvert.DeserializeObject<List<Alarm>>(responseString);
+        
+        return alarms;
+        
     }
 }
