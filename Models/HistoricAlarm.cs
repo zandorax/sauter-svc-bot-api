135169b41d084b8db6cad915238e6bce5d95461b
Dominic Blattner <zandorax@protonmail.com>
2022-01-12 18:05:08 +0100

    Try to deserialize nested json

---
+++ Models/HistoricAlarm.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BotAPI.Models;
+
+public class HistoricAlarm
+{
+    [JsonPropertyName("HisoricAlarms")]
+    public Alarm[] alarms{get; set;}
+    
+    [JsonPropertyName("PageCount")]
+    public string pageNbr { get; set;}
+}
