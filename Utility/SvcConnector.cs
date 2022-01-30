0a1624c7678746044ae0a660ccc4d03c889a3561
Dominic Blattner <zandorax@protonmail.com>
2022-01-30 07:31:18 +0100

    PostAsync added

--- Utility/SvcConnector.cs
+++ Utility/SvcConnector.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
 
 namespace BotAPI.Utility;
 
@@ -29,8 +26,23 @@ public static class SvcConnector
         
         string responseString = await response.Content.ReadAsStringAsync();
         return responseString;
-    } 
-    
+    }
+
+    public static async void PostAsync(string postParam, string content)
+    {
+        await LoginAsync();
+        
+        Client.DefaultRequestHeaders.Accept.Clear();
+        Client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var uri = "http://sautervisioncenter.demo.sauter-bc.com/VisionCenterApiService/api/" + postParam;
+        var requestContent = new StringContent(content, Encoding.UTF8, "application/json");
+        await Client.PostAsync(uri, requestContent);
+        
+    }
+        
+
     private static async Task LoginAsync()
     {
         DotNetEnv.Env.Load();
