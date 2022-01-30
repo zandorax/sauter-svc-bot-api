79297dde2815883b64907628c8ea9de87ea69e8f
Dominic Blattner <zandorax@protonmail.com>
2022-01-30 07:30:53 +0100

    Crated

---
+++ Controllers/AckTransitionController.cs
@@ -0,0 +1,31 @@
+﻿using BotAPI.Models;
+using BotAPI.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BotAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class AckTransitionController : ControllerBase
+{
+    [HttpPost]
+    public async Task PostAckTransition(int objId, int toState, string comment)
+    {
+        DotNetEnv.Env.Load();
+        
+        var request = new AckTransitionDto
+        {
+            ObjectId = objId,
+            ToState = toState,
+            Password = Environment.GetEnvironmentVariable("SVC_PASSWORD"),
+            Comment = comment
+        };
+
+        var body = JsonConvert.SerializeObject(request);
+        
+        SvcConnector.PostAsync("AckTransition", body);
+        
+    }
+}
