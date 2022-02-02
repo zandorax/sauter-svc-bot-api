443fcb06d4b18eebb0ac25d0d8930865cb712811
Dominic Blattner <zandorax@protonmail.com>
2022-02-02 16:13:12 +0100

    example trend implemented

--- Controllers/TrendController.cs
+++ Controllers/TrendController.cs
@@ -3,6 +3,9 @@ using QuickChart;
 
 namespace BotAPI.Controllers;
 
+[ApiController]
+[Route("[controller]")]
+
 public class TrendController : ControllerBase
 {
     [HttpGet]
@@ -13,22 +16,30 @@ public class TrendController : ControllerBase
     private string GetChart()
     {
         double[] data = new double[24];
-        for (int i = 0; i < data.Length; i++)
+        data[0] = 25;
+        for (int i = 1; i < 4; i++)
         {
-            data[i] = 1.24*i;
+            data[i] = 20*i;
         }
+
+        data[4] = 80;
+        for (int i = 5; i < data.Length; i++)
+        {
+            data[i] = 1.968*i;
+        }
+        
         string requestData = String.Join(",", data);
         
         Chart chart = new Chart
         {
-            Width = 250,
-            Height = 150,
+            Width = 500,
+            Height = 300,
             Config = "{\r\n"+
                      "type: 'line',\r\n"             +
                      "data: {\r\n"                   +
-                     "labels: ['Trend', '24h'],\r\n" +
+                     "labels: ['0h','3h','6h','9','12h','15h','18','21h','24h'],\r\n" +
                      "datasets: [{\r\n"              +
-                     "label: 'Temperature',\r\n"     +
+                     "label: 'Mein Beispiel',\r\n"     +
                      "data: ["                       +
                      requestData                     +
                      "]\r\n"                         +
