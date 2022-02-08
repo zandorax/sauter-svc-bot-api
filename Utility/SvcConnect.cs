6b0addc8738c7868cbc4a38fb0e216929a939a9f
Dominic Blattner <zandorax@protonmail.com>
2022-02-08 20:34:01 +0100

    Singelton

---
+++ Utility/SvcConnect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BotAPI.Utility;
+
+public sealed class SvcConnect
+{
+    private SvcConnect(){}
+
+    private static SvcConnect _instance;
+    private static HttpClient Client = new();
+
+    public static SvcConnect GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new SvcConnect();
+        }
+
+        return _instance;
+    }
+
+    private static void SvcLogin()
+    {
+        
+    } 
+
+}
