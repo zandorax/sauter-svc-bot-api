d530d674c21f2f306a7c2395197457765df8a59b
Dominic Blattner <zandorax@protonmail.com>
2022-02-16 15:18:36 +0100

    TrendController.cs can display values from now 24h into the past

--- Controllers/TrendController.cs
+++ Controllers/TrendController.cs
@@ -1,5 +1,6 @@
 ﻿using BotAPI.Models;
 using BotAPI.Utility;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using QuickChart;
@@ -12,15 +13,13 @@ namespace BotAPI.Controllers;
 public class TrendController : ControllerBase
 {
     [HttpGet]
-    /*public string GetTrend()
-    {
-        
-        return DrawChart(2);
-    }*/
     public async Task<string> GetTrendList()
     {
-        long dateTo = 637803936000000000;//DateTime.Now.Ticks;
-        long dateFrom = 637802208000000000;//dateTo;
+        long dateTo = DateTime.Now.Ticks;
+        long dateFrom = dateTo - (TimeSpan.TicksPerHour * 24);
+        
+        
+        
         Task<string> taskString;
         string apiParam = "AggregatedData?" +
                           "options.objectId=120" +
@@ -42,10 +41,16 @@ public class TrendController : ControllerBase
     private string DrawChart(AggregatedDataDto values)
     {
 
-        string lables = "'1','2','3','4,'5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24'";
-        string requestData = string.Join(",", values);
+        string lables = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24";
+        string[] requestDataArray = new String[24];
+        string label = "24h Trend für Objekt";
+        for(int i = 0; i < values.AggregatedDataValues.Count; i++)
+        {
+            requestDataArray[i] = string.Concat(values.AggregatedDataValues[i].AggregatedValue);
+        }
 
-      
+        string requestData;
+        requestData = String.Join(",", requestDataArray);
         
         Chart chart = new Chart
         {
@@ -58,7 +63,7 @@ public class TrendController : ControllerBase
                      lables                          +
                      "],\r\n"                        +
                      "datasets: [{\r\n"              +
-                     "label: 'Mein Beispiel',\r\n"   +
+                     "label: '"+label+"',\r\n"   +
                      "data: ["                       +
                      requestData                     +
                      "]\r\n"                         +
--- Models/AggregatedDataDto.cs
+++ Models/AggregatedDataDto.cs
@@ -11,22 +11,6 @@ public class AggregatedDataDto
 
 public class AggregatedData
 {
-    [JsonIgnore]
-    public long Id { get; set; }
-    [JsonIgnore]
     public int DataObjectID { get; set; }
-    [JsonIgnore]
-    public string CalculationTimestamp { get; set; }
-    
     public float AggregatedValue { get; set; }
-    [JsonIgnore]
-    public string AggregationFunctionName { get; set; }
-    [JsonIgnore]
-    public string AggregationLevelName { get; set; }
-    [JsonIgnore]
-    public string AggregationTypeName { get; set; }
-    [JsonIgnore]
-    public int NrOccurrences { get; set; }
-    [JsonIgnore]
-    public bool IsComplete { get; set; }
 }
