c0a945d6677377ae42de7145d2bd52d49a471f9b
Dominic Blattner <zandorax@protonmail.com>
2022-02-19 15:33:15 +0100

    Output made more legible

--- Models/DataObjectDto.cs
+++ Models/DataObjectDto.cs
@@ -4,10 +4,16 @@ namespace BotAPI.Models;
 
 public class DataObjectDto
 {
+     [JsonIgnore]
      public long ObjectId { get; set; }
+     [JsonIgnore]
      public long PropertyId { get; set; }
+     [JsonIgnore]
      public int Priority { get; set; }
+     [JsonPropertyName("actual-value")]
      public string NewValue { get; set; }
+     [JsonIgnore]
      public string Password { get; set; }
+     [JsonIgnore]
      public string? Comments { get; set; }
 }
