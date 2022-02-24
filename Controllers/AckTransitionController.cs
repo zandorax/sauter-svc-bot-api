4cc1e7aae0b8eaf82064c88cf1f824df7356f47e
Dominic Blattner <zandorax@protonmail.com>
2022-02-24 19:49:47 +0100

    description

--- Controllers/AckTransitionController.cs
+++ Controllers/AckTransitionController.cs
@@ -14,12 +14,14 @@ public class AckTransitionController : ControllerBase
     public async Task<ActionResult> PostAckTransition(int objId, int? fromState, string? comment)
     {
         DotNetEnv.Env.Load();
+        //Status wird von SVC bestimmt. Evt müssen weitere hinzugefügt werden
         int? toState = fromState switch
         {
             3 => null,
             0 => 2,
             _ => 99
         };
+        //Ist kein Status angegeben wird ein Fehler produziert
         if (toState == 99)
         {
            return BadRequest("invalid state");
